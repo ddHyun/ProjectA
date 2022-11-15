fce6dadfc5e6341dd07ae2cf7f1973bfd8acb0c2
ddHyun <ddHyun@github.com>
2022-11-15 13:45:43 +0900

    docs: community
    커뮤니티 세팅중

--- tourGo/src/main/java/org/tourGo/controller/community/ReviewController.java
+++ tourGo/src/main/java/org/tourGo/controller/community/ReviewController.java
@@ -1,13 +1,18 @@
 package org.tourGo.controller.community;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.tourGo.service.community.ReviewService;
 
 @Controller
 @RequestMapping("/community/review")
 public class ReviewController {
+	
+	@Autowired
+	private ReviewService reviewService;
 	
 	@GetMapping("/review_main")
 	public String index(Model model) {
--- tourGo/src/main/java/org/tourGo/service/community/ReviewService.java
+++ tourGo/src/main/java/org/tourGo/service/community/ReviewService.java
@@ -1,5 +1,12 @@
 package org.tourGo.service.community;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.tourGo.models.community.review.ReviewDao;
+
+@Service
 public class ReviewService {
 
+	@Autowired
+	private ReviewDao reviewDao;
 }
