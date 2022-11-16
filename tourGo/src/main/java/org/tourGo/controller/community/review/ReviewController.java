cae12c0bc1c11d1835fd43031e0d668eb223e7e8
ddHyun <ddHyun@github.com>
2022-11-16 16:41:35 +0900

    -cache:false 설정 -여행후기 메인화면 작성중

--- tourGo/src/main/java/org/tourGo/controller/community/review/ReviewController.java
+++ tourGo/src/main/java/org/tourGo/controller/community/review/ReviewController.java
@@ -1,5 +1,8 @@
 package org.tourGo.controller.community.review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -16,8 +19,19 @@ public class ReviewController {
 	private ReviewService reviewService;
 	
 	@GetMapping("/review_main")
-	public String index(Model model) {
-		model.addAttribute("welcome", "안녕하세요!");
+	public String index(Model model) {		
+		
+		List<ReviewRequest> lists = new ArrayList<>();
+		
+		for(int i=1; i<=10; i++) {
+			ReviewRequest reviewRequest = new ReviewRequest();
+			reviewRequest.setName("사용자"+i);
+			reviewRequest.setReviewTitle("제목"+i);
+			reviewRequest.setRead(i+1);
+			lists.add(reviewRequest);
+		}
+		
+		model.addAttribute("lists", lists);
 		return "community/review/review_main";
 	}
 	
