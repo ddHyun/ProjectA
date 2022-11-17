468d5c952e825f25a5296675481572e61cd72e7e
Mun Kyoung-min <mun9659@naver.com>
2022-11-17 23:07:13 +0900

    [feat] 회원가입 약관 페이지, 회원가입 페이지 추가

--- tourGo/src/main/java/org/tourGo/controller/main/user/SignController.java
+++ tourGo/src/main/java/org/tourGo/controller/main/user/SignController.java
@@ -4,20 +4,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.tourGo.service.main.user.LoginService;
+import org.tourGo.service.main.user.SignService;
 
 @Controller
 @RequestMapping("/main")
 public class SignController {
 	
 	@Autowired
-	private LoginService loginService;
+	private SignService signService;
 	
 	private String fixed_url = "views/main/";
 	
-	@GetMapping("/login")
-	public String login() {
-		return fixed_url + "login";
+	@GetMapping("/signCondition")
+	public String signCondition() {
+		return fixed_url + "signCondition";
+	}
+	
+	@GetMapping("/signUp")
+	public String signUp() {
+		return fixed_url + "signUp";
 	}
 	
 }
