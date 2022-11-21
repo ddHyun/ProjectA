6dd89810ff5662527e1f48fadbe910f2e0483efc
ddHyun <ddHyun@github.com>
2022-11-22 00:37:35 +0900

    -여행후기 메인화면 css, controller 메서드 수정

--- tourGo/src/main/resources/static/js/community_common.js
+++ tourGo/src/main/resources/static/js/community_common.js
@@ -1,10 +1,8 @@
 //커뮤니티 각 게시핀에 접근시 상단바에 active클래스 추가하기
 window.addEventListener("DOMContentLoaded", function(){
-	//해당 탬플릿 script에서 보낸 파라미터는 선언없이 사용 가능
 	const boardEl = document.getElementById("board");
 	if (boardEl) {
 		const board = boardEl.value;
 		document.getElementById(board).className += ' active';
 	}
-	console.log("working?");
 });
