69524c6102b54dd5034efe34a4fa428f1928f574
Mun Kyoung-min <mun9659@naver.com>
2022-12-05 17:08:42 +0900

    [Feat] 로그인 실패 시 Boot Security 실패 핸들러 추가

--- tourGo/src/main/java/org/tourGo/controller/user/LoginController.java
+++ tourGo/src/main/java/org/tourGo/controller/user/LoginController.java
@@ -1,13 +1,11 @@
 package org.tourGo.controller.user;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.tourGo.models.entity.user.User;
-import org.tourGo.service.user.LoginService;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
@@ -16,7 +14,13 @@ public class LoginController {
 	private String fixed_url = "user/";
 	
 	@GetMapping("/login")
-	public String login(Model model) {
+	public String login(@RequestParam(value="error", required=false) String error,
+								@RequestParam(value="exception", required=false) String exception
+								, Model model) {
+		
+		// 로그인 실패 시 error 표기 model로 보내줌
+		model.addAttribute("error", error);
+		model.addAttribute("exception", exception);
 		
 		return fixed_url + "login";
 	}
