dd04525fa8927b7ce980e377756825bcb05a6f24
juj0802 <jak2080@naver.com>
2022-12-06 21:54:25 +0900

    plan 관련 객체 수정

---
+++ tourGo/src/main/java/org/tourGo/service/plan/PlanDetailsService.java
@@ -0,0 +1,13 @@
+package org.tourGo.service.plan;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PlanDetailsService {
+
+	@Autowired
+	private PlanDetailsRepository repository;
+	
+	
+}
