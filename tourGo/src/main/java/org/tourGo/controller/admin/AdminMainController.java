f7721fdea8889896b7b38047cb3e821d0c748d8a
Mun Kyoung-min <mun9659@naver.com>
2022-12-09 22:06:49 +0900

    [Feat] 관리자 메인페이지 부트스트랩 관련 CSS, JS 적용 진행

--- tourGo/src/main/java/org/tourGo/controller/admin/AdminMainController.java
+++ tourGo/src/main/java/org/tourGo/controller/admin/AdminMainController.java
@@ -1,8 +1,10 @@
 package org.tourGo.controller.admin;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.tourGo.config.auth.PrincipalDetail;
 
 @Controller
 public class AdminMainController {
@@ -14,11 +16,16 @@ public class AdminMainController {
 	}
 	
 	@GetMapping("/admin/index")
-	public String admin_main(Model model) {
-		model.addAttribute("addCss", new String[] {"vendor/fontawesome-free/css/all.min"});
-		model.addAttribute("addCss", new String[] {"admin/sb-admin-2.min"});
-		model.addAttribute("addScript", new String[] {"admin/sb-admin-2.min"});
-		// model.addAttribute("addScript", new String[] {"admin/sb-admin-2.min"});
+	public String admin_main(Model model,
+											@AuthenticationPrincipal PrincipalDetail principal) {
+		
+		// 부트스트랩 관련 CSS 추가
+		model.addAttribute("addCss", new String[] {"admin/sb-admin-2"});
+		model.addAttribute("addBootstrapCss", new String[] {"fontawesome-free/css/all"});
+		
+		// 부트스트랩 관련 JS 추가
+		// model.addAttribute("addScript", new String[] {"admin/index"});
+		model.addAttribute("addBootstrapJs", new String[] {"jquery/jquery.min", "bootstrap/js/bootstrap.bundle.min", "jquery-easing/jquery.easing.min"});
 		return  "admin/index";
 	}
 }
