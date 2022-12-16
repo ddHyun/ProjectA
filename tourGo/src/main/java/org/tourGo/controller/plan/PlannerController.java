e6fc1f0cc5cee4f056d2508199185b2bf75415f6
juj0802 <jak2080@naver.com>
2022-12-16 18:34:32 +0900

    planner쪽 파일 통합 & 템플릿common폴더 안에 부모창 html이동관련 파일 생성(excution) & common.properties에 타입미스매치 메시지생성

--- tourGo/src/main/java/org/tourGo/controller/plan/PlannerController.java
+++ tourGo/src/main/java/org/tourGo/controller/plan/PlannerController.java
@@ -1,19 +1,28 @@
 	package org.tourGo.controller.plan;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.tourGo.config.auth.PrincipalDetail;
+import org.tourGo.models.plan.PlannerRq;
 import org.tourGo.models.plan.details.PlanDetailsRq;
 import org.tourGo.models.plan.entity.PlanDetails;
+import org.tourGo.models.plan.entity.Planner;
 import org.tourGo.models.plan.entity.PlanDetails.PlanDetailsBuilder;
 import org.tourGo.service.plan.PlanDetailsRepository;
 import org.tourGo.service.plan.PlanDetailsService;
+import org.tourGo.service.plan.PlannerRepository;
 
 import lombok.Value;
 
@@ -21,37 +30,70 @@ import lombok.Value;
 public class PlannerController {
 
 	@Autowired
-	private PlanDetailsRepository rp;
-	@GetMapping("/plan")
-	public String plannerDetails() {
+	private PlanDetailsRepository detailsRepp;
+	
+	@Autowired
+	private PlannerRepository plannerRepo;
+	
+	@GetMapping("/plan") // 여행 상세 일정 보는 화면
+	public String plannerdate(Model model) {
 		
-		
-		return "plan/planDetails";
-	}
-	@GetMapping("/makeplan")
-	public String makePlan(@AuthenticationPrincipal PrincipalDetail pd,Model model) {
-		PlanDetailsRq rq = new PlanDetailsRq();
-		model.addAttribute("planDetails",rq);
-		return "plan/makePlan";
+		PlannerRq plannerRq= new PlannerRq();
+		model.addAttribute("plannerRq",plannerRq);
+		return "plan/plannerView";
 	}
 
-			
-	@PostMapping("/makePlan")
-	public String makePlan22(Model model) {
-		PlanDetailsRq rq = new PlanDetailsRq();
-		rq = ((PlanDetailsRq)model.getAttribute("planDetails"));
-		PlanDetails entity = PlanDetails.builder().address(rq.getAddress()).build();
-		rp.save(entity);
-		return "plan/makePlan";
+	@GetMapping("/makeplan") // 여행 상세 일정 만드는 화면
+	public String makeDate() {
+	
+		return "plan/makeDateView";
 	}
-		
 	
 	@GetMapping("/makeplan2")
-	public String makePlan2(Model model) {
-		PlanDetailsRq rq = new PlanDetailsRq();
-		model.addAttribute("planDetails",rq);
-		return "plan/makePlanTest";
-
+	public String makeDate2(Model model) {
+		PlannerRq plannerRq= new PlannerRq();
+		model.addAttribute("plannerRq",plannerRq);
+		return "plan/makePlan";
+	}
+	
+	@RequestMapping(value="/makeDetails", method=RequestMethod.GET)
+	public String makePlan(Model model) {
+	
+		return "plan/makeDetails";
+	}
+	
+	
+	
+	@PostMapping("/makeDetails")
+	public String makePlanTest(@Valid PlannerRq plannerRq, Errors errors, Model model) {
+		
+		if (errors.hasErrors()) {
+			return "plan/makePlan";
+		}
+		
+		LocalDate sdate = plannerRq.getSdate();
+		Integer _day = plannerRq.getDay();
+		int day = _day == null ? 0 : _day;
+		LocalDate edate = sdate.plusDays(day);
+		Planner planner = Planner.builder().title(plannerRq.getTitle()).day(day).sdate(sdate).edate(edate).memo(plannerRq.getMemo())
+		.planSize(plannerRq.getPlanSize()).planType(plannerRq.getPlanType()).build();
+		
+		plannerRepo.save(planner);
+		//'../plan/makePlan'
+		model.addAttribute("scripts", "parent.location.replace('/makeDetails');");
+		return "common/excution";
+				
+	}
+	
+		
+	
+	@GetMapping("/readplan")
+	public String reddate() {
+		return "plan/read";
+	}
+	@GetMapping("/writeplan")
+	public String writedate() {
+		return "plan/write";
 	}
 		
 }
--- tourGo/src/main/java/org/tourGo/models/plan/PlannerRq.java
+++ tourGo/src/main/java/org/tourGo/models/plan/PlannerRq.java
@@ -1,8 +1,14 @@
 package org.tourGo.models.plan;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotBlank;
+
+import org.springframework.format.annotation.DateTimeFormat;
 import org.tourGo.models.plan.details.PlanDetailsRq;
 import org.tourGo.models.plan.entity.Planner;
 
@@ -18,65 +24,20 @@ import lombok.NoArgsConstructor;
 public class PlannerRq {//여행 일정짜기dto
 
 	 private Long plannerNo;
+	 @NotBlank(message="여행제목을 입력하세요.")
 	 private String title;
-	 private LocalDateTime sdate;
-	 private LocalDateTime edate;
+	 @DateTimeFormat(pattern="yyyy-MM-dd")
+	 private LocalDate sdate;
+	 @DateTimeFormat(pattern="yyyy-MM-dd")
+	 private LocalDate edate;
 	 private String planSize;
-	 private String planType;
+	 private TourType planType;
 	 private String memId;//수정예정
 	 private String memo;
 	 private String image;//대표이미지
-	 private int day;
+	 private Integer day;
 	 
-	 public static PlannerRq entityToPlanner(Planner entity) {
-		 
-		return PlannerRq.builder().plannerNo(entity.getPlannerNo())
-		 .planSize(entity.getPlanSize())
-		 .planType(entity.getPlanType())
-		 .sdate(entity.getSdate())
-		 .edate(entity.getEdate())
-		 .title(entity.getTitle())
-		 .image(entity.getImage())
-		 .memo(entity.getMemo()).build();
- 
-		/** Planner planner = new Planner();
-		 planner.setPlannerNo(entity.getPlannerNo());
-		 planner.setPlanSize(entity.getPlanSize());
-		 planner.setPlanType(entity.getPlanType());
-		 planner.setSdate(entity.getSdate());
-		 planner.setEdate(entity.getEdate());
-		 planner.setTitle(entity.getTitle());
-		 planner.setImage(entity.getImage());
-		// planner.setMemId(entity.getMemId());
-		 planner.setMemo(entity.getMemo());*/
-		
-
-	 }
-	 
-	 public static Planner plannerToEntity(PlannerRq planner) {
-
-		 return Planner.builder().plannerNo(planner.getPlannerNo())
-				 .planSize(planner.getPlanSize())
-				 .planType(planner.getPlanType())
-				 .sdate(planner.getSdate())
-				 .edate(planner.getEdate())
-				 .title(planner.getTitle())
-				 .image(planner.getImage())
-				 .memo(planner.getMemo()).build();
-	/**	 entity.setPlannerNo(planner.getPlannerNo());
-		 entity.setPlanSize(planner.getPlanSize());
-		 entity.setPlanType(planner.getPlanType());
-		 entity.setSdate(planner.getSdate());
-		 entity.setEdate(planner.getEdate());
-		 entity.setTitle(planner.getTitle());
-		 entity.setImage(planner.getImage());
-	//	 entity.setMemId(planner.getMemId());
-		 entity.setMemo(planner.getMemo());
-		 */
-		 
 	
-	 }
-	 
 	 
 	 
 	 
