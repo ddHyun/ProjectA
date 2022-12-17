4b0ecfcf5f06f1b0470fbf1603632e39c0f74628
SiYeonBaek <siyeon092700@gmail.com>
2022-12-17 09:31:37 +0900

    국내여행지 폴더 생성

---
+++ tourGo/src/main/java/org/tourGo/controller/destination/TravelDestinationMainController.java
@@ -0,0 +1,14 @@
+package org.tourGo.controller.destination;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TravelDestinationMainController {
+	
+	@GetMapping("/travel_destination_main")
+	public String travel_dstination_main() {
+		return "travel_destination/travel_destination_main";
+	}
+
+}
