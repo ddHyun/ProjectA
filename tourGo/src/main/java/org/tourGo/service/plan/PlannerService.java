739fd2b40b0e2664f3cf2f8c3d55708553467931
juj0802 <jak2080@naver.com>
2022-12-19 20:12:49 +0900

    planner 엔티티 수정

--- tourGo/src/main/java/org/tourGo/service/plan/PlannerService.java
+++ tourGo/src/main/java/org/tourGo/service/plan/PlannerService.java
@@ -1,5 +1,6 @@
 package org.tourGo.service.plan;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -10,20 +11,46 @@ import org.springframework.stereotype.Service;
 import org.tourGo.models.entity.user.User;
 import org.tourGo.models.plan.PlannerRq;
 import org.tourGo.models.plan.entity.Planner;
+import org.tourGo.models.user.UserRepository;
 
 @Service
 public class PlannerService {
 
 	@Autowired
-	private PlannerRepository repository;
+	private PlannerRepository plannerRepo;
+	@Autowired
+	private UserRepository userRepo;
 	
-	public List<Planner> userPlanner(String userId){ //db로부터 플래너 번호를 내림차순하여 planner List형태로 변환하는 메서드
+	public List<Planner> userPlanner(Long userNo){ //db로부터 플래너 번호를 내림차순하여 planner List형태로 변환하는 메서드
 
 
 		
-		List<Planner> list = repository.findAllByUser(userId,Sort.by(Sort.Direction.DESC,"plannerNo"));
+		List<Planner> list = plannerRepo.findAllByUser(userNo,Sort.by(Sort.Direction.DESC,"plannerNo"));
 
 		return list;
+	}
+	
+	public Planner process(PlannerRq plannerRq,User user) {
+		
+		
+		
+		LocalDate sdate = plannerRq.getSdate();
+		Integer day = plannerRq.getDay();
+		LocalDate edate = sdate.plusDays(day);
+		Planner planner = Planner.builder().title(plannerRq.getTitle()).day(day).sdate(sdate).edate(edate).memo(plannerRq.getMemo())
+		.planSize(plannerRq.getPlanSize()).planType(plannerRq.getPlanType()).user(user).build();
+	
+		planner = plannerRepo.save(planner);
+		return planner;
+		
+	}
+	
+	public Planner getPlanner(Long id) {
+		
+		Planner planner = plannerRepo.findById(id).orElse(null);
+		
+		
+		return planner;
 	}
 	
 	
