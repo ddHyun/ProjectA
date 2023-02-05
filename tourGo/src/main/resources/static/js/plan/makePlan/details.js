d16c3e85dc5841549a6c93aea97f158e47089b62
isj003 <ldb1910@gmail.com>
2023-02-05 17:59:28 +0900

    플래너 ccs 수정

--- tourGo/src/main/resources/static/js/plan/makePlan/details.js
+++ tourGo/src/main/resources/static/js/plan/makePlan/details.js
@@ -171,7 +171,7 @@ function createItem(result, parentEl) {
 				marker.setMap(map);
 				// 마커를 클릭했을 때 마커 위에 표시할 인포윈도우를 생성합니다
 				var iwContent = `<div style="padding:5px;">${infoTitle}</div>
-				<img src="${infoImg}" onerror="this.onerror=null; this.src='../../../../images/noimg.jpg';"></img>`
+				<img class="imgview" src="${infoImg}" onerror="this.onerror=null; this.src='../../../../images/noimg.jpg';"></img>`
 								, // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
    				 iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
 
