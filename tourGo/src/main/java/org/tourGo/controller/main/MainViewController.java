18b19a624647c220d7469f297a09a83a5a416103
SiYeonBaek <siyeon092700@gmail.com>
2023-02-28 17:11:35 +0900

    수정4

--- tourGo/src/main/java/org/tourGo/controller/main/MainViewController.java
+++ tourGo/src/main/java/org/tourGo/controller/main/MainViewController.java
@@ -12,61 +12,70 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.tourGo.config.auth.PrincipalDetail;
+import org.tourGo.models.destination.entity.DestinationDetail;
 import org.tourGo.models.entity.user.User;
 import org.tourGo.models.plan.entity.Planner;
 import org.tourGo.models.user.UserRepository;
+import org.tourGo.service.destination.DestinationMainService;
 import org.tourGo.service.plan.PlannerService;
-
 
 @Controller
 public class MainViewController {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private PlannerService plannerService;
+	
+	@Autowired
+	private DestinationMainService destinationMainService;
 
 	@GetMapping("/main_view")
 	public String main_view(Model model) {
-		
+
 		return "main/main_view";
 	}
-	
+
 	@GetMapping("/main_view2")
 	public String main_view2(Model model) {
-		
+
 		return "main/main_view2";
 	}
-	
+
 	@GetMapping("/main_view3")
-	public String main_view3(Model model,@AuthenticationPrincipal PrincipalDetail principal
-			) {
+	public String main_view3(Model model, @AuthenticationPrincipal PrincipalDetail principal) {
 		Optional<User> _user = null;
-		
-			List<Planner> top3 = new ArrayList<>();	
-			try {
-			top3 = plannerService.topLikedPlanner();
-			}catch(Exception e){
-				top3=null;
-			}
-			model.addAttribute("plannerTop3List", top3);
-		
-		
+
+		List<Planner> top3 = new ArrayList<>();
 		try {
-		_user = userRepository.findByUserId(principal.getUser().getUserId());
-		User user = _user.orElse(null);		
-		model.addAttribute("user", user);
-		System.out.println("안녕하세요"+user);
+			top3 = plannerService.topLikedPlanner();
+		} catch (Exception e) {
+			top3 = null;
+		}
+		model.addAttribute("plannerTop3List", top3);
 		
-		model.addAttribute("addScript", "layer");
-		return "main/main_view3";
-	
-		
+		List<DestinationDetail> top3_2 = new ArrayList<>();
+		try {
+			top3_2 = destinationMainService.topHitsDestination();
+		} catch (Exception e) {
+			top3_2 = null;
+		}
+		model.addAttribute("destTop3List",top3_2);
+
+		try {
+			_user = userRepository.findByUserId(principal.getUser().getUserId());
+			User user = _user.orElse(null);
+			model.addAttribute("user", user);
+			System.out.println("안녕하세요" + user);
+
+			model.addAttribute("addScript", "layer");
+			return "main/main_view3";
+
 		} catch (Exception e) {
 			return "main/main_view3";
 		}
 
 	}
-	
+
 }
